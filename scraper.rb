57f43fbf05b3d0da1b59ea9e4d0cef2401259553
Tony Bowden <tony@tmtm.com>
2016-09-21 09:08:19 +0100

    Remove trailing spaces

--- scraper.rb
+++ scraper.rb
@@ -3,39 +3,39 @@
 
 require 'wikidata/fetcher'
 
-directora_55 = EveryPolitician::Wikidata.wikipedia_xpath( 
+directora_55 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://pt.wikipedia.org/wiki/Lista_de_deputados_federais_do_Brasil_da_55.ª_legislatura',
   after: '//span[@id="Mesa_Diretora"]',
   before: '//span[@id=".C2.A0Acre"]',
   xpath: '//table//td[3]//a[not(@class="new")]/@title',
-) 
+)
 
-by_area_55 = EveryPolitician::Wikidata.wikipedia_xpath( 
+by_area_55 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://pt.wikipedia.org/wiki/Lista_de_deputados_federais_do_Brasil_da_55.ª_legislatura',
   after: '//span[@id=".C2.A0Acre"]',
   before: '//span[@id="Licen.C3.A7as"]',
   xpath: '//table//td[1]//a[not(@class="new")]/@title',
-) 
+)
 
-licencas_55 = EveryPolitician::Wikidata.wikipedia_xpath( 
+licencas_55 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://pt.wikipedia.org/wiki/Lista_de_deputados_federais_do_Brasil_da_55.ª_legislatura',
   after: '//span[@id="Licen.C3.A7as"]',
   before: '//span[@id="Ver_tamb.C3.A9m"]',
   xpath: '//table//td[2]//a[not(@class="new")]/@title',
-) 
+)
 
-directora_54 = EveryPolitician::Wikidata.wikipedia_xpath( 
+directora_54 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://pt.wikipedia.org/wiki/Lista_de_deputados_federais_do_Brasil_da_54.ª_legislatura',
   after: '//span[@id="Mesa_Diretora"]',
   before: '//span[@id=".C2.A0Acre"]',
   xpath: '//table//td[3]//a[not(@class="new")]/@title',
-) 
+)
 
-by_area_54 = EveryPolitician::Wikidata.wikipedia_xpath( 
+by_area_54 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://pt.wikipedia.org/wiki/Lista_de_deputados_federais_do_Brasil_da_54.ª_legislatura',
   after: '//span[@id=".C2.A0Acre"]',
   before: '//span[@id="Licen.C3.A7as"]',
   xpath: '//table//td[1]//a[not(@class="new")]/@title',
-) 
+)
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { pt: directora_55 | by_area_55 | licencas_55 | directora_54 | by_area_54 })
